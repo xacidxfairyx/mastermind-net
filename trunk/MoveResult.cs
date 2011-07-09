fc373d110de4b6aebd80c99d0fe65e6fca6955ec
asilva.sc <asilva.sc@0ebd046e-02d1-29e4-0d0e-f7235dfd0c1b>
2011-07-09 20:16:04 +0000

    Added Board implementation. The Mastermind factory now allows the creation of custom games. MastermindGame now have game status and difficulty levels. Created MoveResult to give final users feedback of moves. Minor documentation fix.

---
+++ trunk/MoveResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Softklin.Mastermind
+{
+    /// <summary>
+    /// Represents the result of a move
+    /// </summary>
+    public struct MoveResult
+    {
+        /// <summary>
+        /// Get the total of pegs in both right position and color.
+        /// </summary>
+        /// <remarks>The peg associated with this is normally black</remarks>
+        public int TotalRightColorAndPosition { get; private set; }
+
+        /// <summary>
+        /// Get the total of pegs in with right colors but wrong positions
+        /// </summary>
+        /// <remarks>The peg associated with this is blank</remarks>
+        public int TotalRightColor { get; private set; }
+
+        /// <summary>
+        /// Get the total number of tries made so far (also the current move number)
+        /// </summary>
+        public int TotalMoves { get; private set; }
+    }
+}
